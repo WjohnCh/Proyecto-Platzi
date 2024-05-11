fd5fe305271ea85254c623833c15dbc16aac6b39
WjohnCh22 <wjohnch22@gmail.com>
2024-05-11 18:10:26 -0500

    Funcionalidad Navbar/Mobile Archivos delete

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 document.addEventListener("DOMContentLoaded", function(){
-    const flechaNavbar = document.getElementById('narvar-right-a')
+    const flechaNavbar = document.getElementById('narvar-right-a');
+    const btnAbrirCerrar = document.getElementById('menu-navbar-mobile');
+    const desktopMenu = document.getElementById("desktop-menu");
+    const mobileMenu = document.querySelector(".menu-principal-mobile");
+    
     flechaNavbar.addEventListener('click', AbrirDesktop);
     function AbrirDesktop(){
-        const desktopMenu = document.getElementById("desktop-menu");
         desktopMenu.classList.toggle("inactive");
     }
+    
+    btnAbrirCerrar.addEventListener('click', AbrirMobile);
+    function AbrirMobile(){
+        mobileMenu.classList.toggle("inactive");
+    }
+
 });
 
 
