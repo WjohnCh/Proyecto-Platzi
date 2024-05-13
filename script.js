60669d237356dce57e3adbdfba6bcfc7ae2c9a20
WjohnCh22 <wjohnch22@gmail.com>
2024-05-12 19:30:07 -0500

    + automaticacion de visualizacion de productos

--- script.js
+++ script.js
@@ -1,4 +1,41 @@
 document.addEventListener("DOMContentLoaded", function(){
+
+    class Product{
+        constructor({nombre, precio}){
+            this.nombre = nombre;
+            this.precio = precio;            
+        }
+    }
+    
+    let productoConjunto = [];
+    const divContenedor = document.querySelector('.cards-container');
+    const divGeneral = document.querySelector('.product-card');
+    
+    function MostrarProductos(){
+        for (productoUnico of productoConjunto){
+            CrearEstructuraObjeto(productoUnico);
+        }
+    }
+    
+    function CrearEstructuraObjeto(producto){
+        const nuevoProducto = divGeneral.cloneNode(true);
+        const precioProducto = nuevoProducto.querySelector('.Precio');
+        const descripcionProducto = nuevoProducto.querySelector('.Descripcion');
+        nuevoProducto.removeAttribute('style');
+        precioProducto.innerText = producto.precio;
+        descripcionProducto.innerText = producto.nombre;
+        divContenedor.append(nuevoProducto);
+        console.log(nuevoProducto);
+    }
+    function CrearObjeto(nombre, precio){
+        const producto = new Product({nombre: nombre, precio: ("$" + precio)});
+        productoConjunto.push(producto);
+        CrearEstructuraObjeto(producto);
+    }
+    
+    console.log(CrearObjeto("Bike", "35.00"));
+
+
     const flechaNavbar = document.getElementById('narvar-right-a');
     const btnAbrirCerrar = document.getElementById('menu-navbar-mobile');
     const desktopMenu = document.getElementById("menu-desktop-desplegable");
